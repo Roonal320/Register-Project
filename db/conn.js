23cbee659a8ce0917e553e91091c9c5208896bf5
Roonal Khandelwal <roonalkhandelwal274@gmail.com>
2022-08-05 21:57:09 +0530

    database correction test

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 
-const db=process.env.DATABASE;
+// const db=process.env.DATABASE;
 
-mongoose.connect(db,{
+mongoose.connect("mongodb+srv://Roonalkhandelwal:kalibilli@cluster0.z59yb.mongodb.net/Register?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{console.log("connection successfull")
